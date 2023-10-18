8fe0a57a8b2450ab6dd9649552f95c6df26c10d4
Royi <soyelmanagerrojo@email.com>
2023-10-18 13:55:34 -0500

    p a href i icono

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,28 @@
+$(document).ready(function(){
+	$(".owl-carousel").owlCarousel({
+		items: 5, // Cambia el número de elementos visibles
+		loop: true,
+		margin: 10,
+		nav: false,
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:1,
+				nav:false
+			},
+			768:{
+				items:3,
+				nav:false
+			},
+			992:{
+				items:5,
+				nav:false
+			}
+		}
+	});
+});
+
+
 AOS.init({
 	duration: 800,
 	easing: 'slide',
